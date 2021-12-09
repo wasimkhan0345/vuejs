7f458646144fef82c60a3ca583e67b7bfdc1914e
unknown <wasim.khan0345@gmail.com>
2021-12-09 14:33:37 +0500

    Wonderful Quote

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,50 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <app-header :maxQuote="maxQuotes" :quoteCount="quotes.length"></app-header>
+    <app-new-quote @quoteAdded="newquote" ></app-new-quote>
+    <app-qoute-grid :quotes="quotes" @QuoteDelete="quoteDeleted"></app-qoute-grid>
+    <div class="row">
+      <div class="col-lg-12 text-center">
+        <div class="alert alert-info">Info: Click on a Quote to delete it!</div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import QuoteGrid from './components/QuoteGrid.vue';
+import NewQuote from './components/NewQuote.vue';
+import HeaderQuote from './components/Header.vue'
+
+
 
 export default {
-  name: 'App',
+  data: function() {
+    return {
+      quotes: ['Just a quote to see something'],
+      maxQuotes: 10
+    }
+  },
   components: {
-    HelloWorld
+    appQouteGrid: QuoteGrid,
+    appNewQuote: NewQuote,
+    appHeader: HeaderQuote
+  },
+  methods: {
+    newquote(quote) {
+      if(this.quotes.length >= this.maxQuotes) {
+        return alert("Please delete quote first!");
+      }
+      this.quotes.push(quote);
+    },
+    quoteDeleted(index) {
+      console.log(index);
+      this.quotes.splice(index,1);
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="row">
+        <div class="col-lg-12">
+            <label for="">Quote</label>
+            <div class="progress">
+                <div :style="{ width: (quoteCount / maxQuote) * 100 + '%'}" class="progress-bar" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100">{{ quoteCount }} / {{ maxQuote }}</div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ["maxQuote","quoteCount"]
+    }
+</script>
+
